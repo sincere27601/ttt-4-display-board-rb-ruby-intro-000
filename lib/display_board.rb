b99626a66c5764f1e1db795e98665f8b28f1118a
Michael Raysor <michaelraysor1@gmail.com>
2021-03-11 14:41:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 
 def display_board
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  puts "#{tic[0]}", " #{tac[2]}", " #{toe[3]}", "#{ex[x]}"
+  puts "#{tic[0]}", " #{tac[2]}", " #{toe[3]}", "#{ex[4]}"
 
 end
