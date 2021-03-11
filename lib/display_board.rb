88e24f53b287319a6698e67649ad69d30036dd2b
Michael Raysor <michaelraysor1@gmail.com>
2021-03-11 15:08:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
  def display_board(board)
   puts board = [" ", " ", " ", " ", "x", " ", " ", " ", " "]
-    
+
   puts "The first box is#{tic[0]}", " The third box is #{tac[2]}", " #The fourth box is {toe[3]}", "The fifth box is #{ex[4]}"
   puts row =  "   |   |   "
   puts seperator = "-----------"
